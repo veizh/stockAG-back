9c7c34f6b7c60310fb14565e5458cdafedd012aa
Veizh <maximedurville95300@gmail.com>
2025-07-03 06:00:22 +0200

    ajout

--- routes/products.js
+++ routes/products.js
@@ -4,16 +4,16 @@ var router = express.Router();
 const auth = require('../middleware/auth');
 /* products listing. */
 //ajouter auth sur getOne ref =>employe / update => admin/updateqtonly => employe/ deleteone=>admin
-router.get('/getOne/:ref', auth,products.getOne);
+router.get('/getOne/:_id', auth,products.getOne);
 router.get('/getAll', products.getAll);
 router.post('/create',auth, products.create);
-router.put('/updateOne/:ref',auth, products.updateOne);
-router.put('/returnFromInter/:ref', products.returnFromInter);
-router.put('/updateImage/:ref',auth, products.updateImage);
-router.put('/updateQuantityAndAlert/:ref',auth, products.updateQuantityAndAlert)
-router.put('/addProductAndHandleAlert/:ref', products.addProductAndHandleAlert)
-router.put('/removeProductAndHandleAlert/:ref',auth, products.removeProductAndHandleAlert)
-router.delete('/deleteOne/:ref',auth, products.deleteOne);
+router.put('/updateOne/:_id',auth, products.updateOne);
+router.put('/returnFromInter/:_id', products.returnFromInter);
+router.put('/updateImage/:_id',auth, products.updateImage);
+router.put('/updateQuantityAndAlert/:_id',auth, products.updateQuantityAndAlert)
+router.put('/addProductAndHandleAlert/:_id', products.addProductAndHandleAlert)
+router.put('/removeProductAndHandleAlert/:_id',auth, products.removeProductAndHandleAlert)
+router.delete('/deleteOne/:_id',auth, products.deleteOne);
 
 // authorize route to roll
 
